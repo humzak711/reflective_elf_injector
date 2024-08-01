3ce53b51881cdc36ce13a6e731b2aa71ecf3ed41
Humza <139543666+humzak711@users.noreply.github.com>
2024-08-01 19:19:44 +0100

    Update main.c
    Simplified comments to be easier to understand for beginners

--- lib/main.c
+++ lib/main.c
@@ -13,17 +13,17 @@
 
 int main() {
     
-   // Call the memfd_create subroutine to create an anonymous file descriptor
+    // Call the memfd_create subroutine to create a file descriptor residing in memory
     int anon_fd = memfd_create("", MFD_CLOEXEC);
     if (anon_fd == -1) {
         perror("memfd_create");
         exit(EXIT_FAILURE);
     }
 
-    // Write the elf to the anonymous file descriptor to write the elf into memory
+    // Write the elf to the file descriptor that is in memory to write the elf into memory
     write(anon_fd, elf, elf_len);
 
-    // Construct a string variable to hold the anonymous file descriptor's path
+    // Construct a string variable to hold the file descriptor's path
     char *anon_fd_path;
     /* To do: construct the filepath to the path of the file descriptor for FreeBSD 
        as currently it constructs the filepath to paths of file descriptor's for linux,
@@ -31,13 +31,13 @@ int main() {
        be in FreeBSD */
     asprintf(&anon_fd_path, "/proc/self/fd/%i", anon_fd);
 
-    // Call execl subroutine on the anonymous file descriptor's path to execute the elf
+    // Call execl subroutine on the file descriptor's path to execute the elf into memory
     if (execl(anon_fd_path, NULL) == -1) {
         perror("execl");
         exit(EXIT_FAILURE);
     }
 
     /* The code here won't be executed if successful because the main thread of the process that executed 
-       the elf in memory will now hold the main thread of the elf executed in memory */
+       the elf into memory will now hold the main thread of the elf executed into memory */
     return EXIT_SUCCESS;
 }
