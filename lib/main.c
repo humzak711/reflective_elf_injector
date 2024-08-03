006772f1dd46f725833aed4f19b04999a76082e0
Ethan Blackwell <ethanblackwellbusiness@protonmail.com>
2024-08-03 00:51:57 +0000

    Update main.c

--- lib/main.c
+++ lib/main.c
@@ -8,12 +8,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <signal.h>
-
-void segfault_handler(int signum) {
-    fprintf(stderr, "Segmentation fault occurred! Signal: %d\n", signum);
-    exit(EXIT_FAILURE);
-}
 
 int main() {
     // Check if elf_len is valid
@@ -39,18 +33,27 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Define a function pointer to execute the ELF binary
-    typedef void (*func_elf_type)();
-    func_elf_type func_elf = (func_elf_type)exec_mem_elf;
+    // Fork a new process
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        munmap(exec_mem_elf, elf_len);
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        // Child process: execute the ELF binary using execve()
+        char *args[] = { NULL };
+        if (execve(exec_mem_elf, args, NULL) == -1) {
+            perror("execve");
+            munmap(exec_mem_elf, elf_len);
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        // Parent process: wait for the child to finish
+        int status;
+        waitpid(pid, &status, 0);
+    }
 
-    // Set up a signal handler for segmentation faults
-    signal(SIGSEGV, segfault_handler);
-
-    // Execute the ELF in memory
-    printf("Executing ELF in memory at address: %p\n", (void *)func_elf);
-    func_elf(); // Attempt to execute the ELF binary
-
-    // Cleanup (should not reach here if execution is successful)
+    // Cleanup
     munmap(exec_mem_elf, elf_len);
     return EXIT_SUCCESS;
 }
