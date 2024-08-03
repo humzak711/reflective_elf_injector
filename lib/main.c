90ea5fa3cf530fddcabfee53bf1152bcb1efd9f8
Ethan Blackwell <ethanblackwellbusiness@protonmail.com>
2024-08-03 00:23:59 +0000

    Update main.c

--- lib/main.c
+++ lib/main.c
@@ -8,16 +8,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <signal.h>
+
+void segfault_handler(int signum) {
+    fprintf(stderr, "Segmentation fault occurred! Signal: %d\n", signum);
+    exit(EXIT_FAILURE);
+}
 
 int main() {
-    // Allocate memory for the ELF binary to hold our elf in memory
+    // Check if elf_len is valid
+    if (elf_len == 0) {
+        fprintf(stderr, "Error: elf_len is zero. No ELF binary to execute.\n");
+        return EXIT_FAILURE;
+    }
+
+    // Allocate memory for the ELF binary
     void *exec_mem_elf = mmap(NULL, elf_len, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (exec_mem_elf == MAP_FAILED) {
         perror("mmap");
         exit(EXIT_FAILURE);
     }
 
-    // Copy the ELF binary into the allocated memory so our elf is held in exec_mem_elf
+    // Copy the ELF binary into the allocated memory
     memcpy(exec_mem_elf, elf, elf_len);
 
     // Change the memory protection to executable
@@ -27,22 +39,18 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    /* Define a function pointer holding our elf so that we can execute our elf in memory 
-        by calling a function */
+    // Define a function pointer to execute the ELF binary
     typedef void (*func_elf_type)();
     func_elf_type func_elf = (func_elf_type)exec_mem_elf;
 
+    // Set up a signal handler for segmentation faults
+    signal(SIGSEGV, segfault_handler);
+
     // Execute the ELF in memory
-    printf("Executing elf in memory at address: %p\n", (void *)func_elf);
-    func_elf(); // To do: fix segfault/core dump when the elf is executed
+    printf("Executing ELF in memory at address: %p\n", (void *)func_elf);
+    func_elf(); // Attempt to execute the ELF binary
 
-    /* Code from here onwards should not be executed if the elf was 
-       successfully executed in memory since our process will now 
-       hold the instance of the elf running in memory */
-
-    // Cleanup
+    // Cleanup (should not reach here if execution is successful)
     munmap(exec_mem_elf, elf_len);
-
     return EXIT_SUCCESS;
 }
-
